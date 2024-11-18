3ed13b0058773b0751d2eb8cccc52004f5416c90
Ralex <95540504+Ralex91@users.noreply.github.com>
2024-11-18 14:56:48 +0100

    chore(): first commit

---
+++ public/game.js
@@ -0,0 +1,199 @@
+const socket = io.connect()
+
+let width, height
+let player = null
+let players = {}
+let foodItems = []
+let mousePosition = { x: 0, y: 0 }
+let directionAngle = 0
+let targetScale = 5
+let currentScale = 10
+const GAME_SIZE = 3000
+const maxSpeed = 2.4
+const zoomSpeed = 0.05
+
+const canvas = document.getElementById("gameCanvas")
+const ctx = canvas.getContext("2d")
+
+const score = document.getElementById("score")
+const position = document.getElementById("position")
+const leaderboardList = document.getElementById("leaderboardList")
+
+socket.on("connect", () => {
+  socket.on("init", (data) => {
+    players = data.players
+    player = data.players[socket.id]
+
+    foodItems = data.foodItems
+
+    setInterval(updateGame, 15)
+    drawGame()
+  })
+
+  socket.on("playersTick", (data) => {
+    players = data
+  })
+
+  socket.on("foodTick", (data) => {
+    foodItems = data
+  })
+
+  socket.on("eatFood", (data) => {
+    player.radius = data
+  })
+})
+
+const resize = () => {
+  width = window.innerWidth
+  height = window.innerHeight
+
+  canvas.width = width
+  canvas.height = height
+}
+
+window.addEventListener("resize", resize)
+resize()
+
+canvas.addEventListener("mousemove", function (event) {
+  mousePosition = { x: event.clientX, y: event.clientY }
+})
+
+const calculateDistance = (x1, y1, x2, y2) =>
+  Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+const updateHud = () => {
+  score.innerHTML = `<b>Score</b>: ${player.radius.toFixed(2)}`
+
+  position.innerHTML = `<b>Position</b>: X${player.x.toFixed(
+    2
+  )}, Y${player.y.toFixed(2)}`
+
+  leaderboardList.innerHTML = ""
+
+  Object.values(players)
+    .sort((a, b) => b.radius - a.radius)
+    .forEach((player) => {
+      leaderboardList.innerHTML += `
+      <li class="flex gap-2 items-center justify-between">
+        <div class="flex gap-2 items-center">
+          <div
+            style="background-color: ${player.color}"
+            class="h-4 w-4 mt-1 rounded-full">
+          </div>
+          <span class="font-bold" style="color: ${player.id === socket.id ? "orange" : "white"}">
+            ${player.name}
+          </span>
+          </div>
+        <p>${player.radius.toFixed(2)}</p>
+      </li>`
+    })
+}
+
+const updateGame = () => {
+  if (!socket.id) {
+    return
+  }
+
+  directionAngle = Math.atan2(
+    mousePosition.y - height / 2,
+    mousePosition.x - width / 2
+  )
+
+  const distance = calculateDistance(
+    width / 2,
+    height / 2,
+    mousePosition.x,
+    mousePosition.y
+  )
+  const speed = Math.min(maxSpeed, distance / 15)
+
+  player.x += Math.cos(directionAngle) * speed
+  player.y += Math.sin(directionAngle) * speed
+
+  player.x = Math.max(
+    player.radius,
+    Math.min(player.x, GAME_SIZE - player.radius)
+  )
+  player.y = Math.max(
+    player.radius,
+    Math.min(player.y, GAME_SIZE - player.radius)
+  )
+
+  updateHud()
+
+  socket.emit("move", {
+    x: player.x,
+    y: player.y,
+  })
+
+  targetScale = 1 / (player.radius / 80)
+  currentScale += (targetScale - currentScale) * zoomSpeed
+}
+
+const drawPlayer = (playerData) => {
+  const radius = players[playerData.id].radius
+
+  ctx.fillStyle = playerData.color
+  ctx.beginPath()
+  ctx.arc(playerData.x, playerData.y, radius, 0, Math.PI * 2)
+  ctx.fill()
+
+  ctx.fillStyle = "white"
+  ctx.textAlign = "center"
+  ctx.textBaseline = "middle"
+  ctx.font = `${radius / 2}px Arial`
+  ctx.strokeStyle = "black"
+  ctx.lineWidth = radius / 14
+  ctx.strokeText(playerData.name, playerData.x, playerData.y)
+  ctx.fillText(playerData.name, playerData.x, playerData.y)
+  ctx.closePath()
+}
+
+const drawFood = () => {
+  for (const food of foodItems) {
+    ctx.beginPath()
+    ctx.fillStyle = food.color
+    ctx.arc(food.x, food.y, 4, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+  }
+}
+
+const drawGrid = () => {
+  ctx.beginPath()
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.3)"
+  ctx.lineWidth = 0.2
+  for (let i = 0; i < GAME_SIZE; i += 10) {
+    ctx.moveTo(i, 0)
+    ctx.lineTo(i, GAME_SIZE)
+    ctx.moveTo(0, i)
+    ctx.lineTo(GAME_SIZE, i)
+  }
+  ctx.stroke()
+  ctx.closePath()
+}
+
+const drawGame = () => {
+  if (!socket.id) {
+    return
+  }
+
+  requestAnimationFrame(drawGame)
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.save()
+
+  ctx.translate(width / 2, height / 2)
+  ctx.scale(currentScale, currentScale)
+  ctx.translate(-player.x, -player.y)
+
+  drawGrid()
+  drawFood()
+
+  for (const id in players) {
+    const playerData = id === socket.id ? player : players[id]
+    drawPlayer(playerData)
+  }
+
+  ctx.restore()
+}
---
+++ server.js
@@ -0,0 +1,121 @@
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+})
+
+const SERVER_PORT = 3000
+const GAME_SIZE = 3000
+const FOODS_COUNT = 500
+const FOOD_EAT = 0.4
+const FOOD_SIZE = 4
+let players = {}
+let foodItems = []
+
+app.use(express.static("public"))
+
+const spawnFood = () => {
+  foodItems.push({
+    id: new Date().getTime(),
+    color: `hsl(${Math.random() * 360}, 50%, 50%)`,
+    x: Math.random() * GAME_SIZE,
+    y: Math.random() * GAME_SIZE,
+  })
+}
+
+const calDistance = (x1, y1, x2, y2) =>
+  Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+const checkFood = (player) => {
+  foodItems.forEach((food, index) => {
+    const distance = calDistance(player.x, player.y, food.x, food.y)
+
+    if (distance < player.radius + FOOD_SIZE / 2) {
+      player.radius += FOOD_EAT
+      foodItems = foodItems.filter((_, i) => i !== index)
+      spawnFood()
+
+      io.to(player.id).emit("eatFood", player.radius)
+      io.emit("foodTick", foodItems)
+    }
+  })
+}
+
+const checkPlayer = (player) => {
+  Object.values(players).forEach((otherPlayer) => {
+    if (player.id !== otherPlayer.id) {
+      const distance = calDistance(
+        player.x,
+        player.y,
+        otherPlayer.x,
+        otherPlayer.y
+      )
+
+      if (distance < player.radius + otherPlayer.radius / 2) {
+        player.radius += otherPlayer.radius
+
+        delete players[otherPlayer.id]
+      }
+    }
+  })
+}
+
+const newPlayer = (id) => ({
+  id: id,
+  name: `Player ${id.slice(0, 4)}`,
+  color: `hsl(${Math.random() * 360}, 50%, 50%)`,
+  x: Math.random() * GAME_SIZE,
+  y: Math.random() * GAME_SIZE,
+  radius: 16,
+})
+
+// Generate initial food items
+for (let i = 0; i < FOODS_COUNT; i++) {
+  spawnFood()
+}
+
+io.on("connection", (socket) => {
+  console.log(`Player connected: ${socket.id}`)
+
+  players[socket.id] = newPlayer(socket.id)
+  socket.emit("init", { players, foodItems })
+
+  socket.on("move", (data) => {
+    // Check if the player is still in the game
+    if (players[socket.id]) {
+      players[socket.id].x = Math.max(
+        players[socket.id].radius,
+        Math.min(data.x, GAME_SIZE - players[socket.id].radius)
+      )
+      players[socket.id].y = Math.max(
+        players[socket.id].radius,
+        Math.min(data.y, GAME_SIZE - players[socket.id].radius)
+      )
+    }
+  })
+
+  socket.on("disconnect", () => {
+    console.log(`Player disconnected: ${socket.id}`)
+
+    delete players[socket.id]
+  })
+})
+
+setInterval(() => {
+  Object.entries(players).forEach(([_, player]) => {
+    checkFood(player)
+    checkPlayer(player)
+  })
+
+  io.emit("playersTick", players)
+}, 15)
+
+server.listen(SERVER_PORT, () => {
+  console.log(`Server started on port ${SERVER_PORT}`)
+})
